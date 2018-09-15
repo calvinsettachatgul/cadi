30c44e883ef86ca7a1d37f349bf026e43b21dcd6
Calvin Settachatgul <calvin.settachatgul@autodesk.com>
2018-09-15 13:44:54 -0700

    ruby class

---
+++ ruby_class/person.rb
@@ -0,0 +1,36 @@
+class Person
+
+  @@number_people = 0
+
+  attr_accessor :first_name, :last_name
+
+  def self.number_people
+    @@number_people
+  end
+
+  def initialize(first_name, last_name)
+    @first_name = first_name
+    @last_name = last_name
+    @@number_people = @@number_people + 1
+  end
+
+  def printme()
+    puts "#{self.first_name} #{self.last_name}"
+  end
+
+  def self.sayHi()
+    puts "Hi"
+  end
+
+end
+
+calvin = Person.new("calvin", "settachatgul")
+someone = Person.new("fist", "last")
+
+puts calvin
+calvin.printme()
+
+# class method
+Person.sayHi()
+
+puts Person.number_people
